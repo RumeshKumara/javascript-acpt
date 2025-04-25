f4e63d93514a7d617ddbb0e6d67c3d418c188e83
rumesh <rumeshk066@gmail.com>
2025-04-25 11:23:40 +0530

    first commit

---
+++ day-1/main.js
@@ -0,0 +1,15 @@
+// function calculateBMI(name, weight, height) {
+
+//     var bmi = weight / (height * height);
+
+//     console.log(`${name} Your BMI is ${bmi} kg/m².`);
+
+// }
+// calculateBMI("Rumesh", 54, 1.8);
+
+
+let multiNumbers = (num1, num2, num3) => {
+    return num1 * num2 * num3;
+}
+
+console.log(multiNumbers(10, 30, 5));
